79816b82f8dc49c6a5dd8c645ca8e3c6d8a57158
wooos <819844849@qq.com>
2022-03-30 18:47:34 +0800

    完成第二十三章学习

---
+++ app/api/__init__.py
@@ -0,0 +1,6 @@
+import imp
+from flask import Blueprint
+
+bp = Blueprint('api', __name__)
+
+from app.api import users, errors, tokens
---
+++ app/api/tokens.py
@@ -0,0 +1,41 @@
+from flask import jsonify
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from app.models import User
+from app.api.errors import error_response
+from app.api import bp
+from app import db
+
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
+
+@basic_auth.error_handler
+def basic_auth_error(status):
+    return error_response(status)
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+@token_auth.error_handler
+def token_auth_error(status):
+    return error_response(status)
+
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    token = basic_auth.current_user().get_token()
+    db.session.commit()
+    return jsonify({'token': token})
+
+@bp.route('/tokens', methods=['DELETE'])
+@token_auth.login_required
+def revoke_token():
+    token_auth.current_user().revoke_token()
+    db.session.commit()
+    return '', 204
